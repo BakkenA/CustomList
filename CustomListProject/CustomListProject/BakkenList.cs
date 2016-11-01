5a0d6cd790d3ba7322489c7a214d60b31a9843fe
Adam Bakken <Adam Bakken>
2016-11-01 16:33:48 -0500

    An add method has been added

--- CustomListProject/CustomListProject/BakkenList.cs
+++ CustomListProject/CustomListProject/BakkenList.cs
@@ -8,6 +8,23 @@ namespace CustomListProject
 {
     public class BakkenList<T>
     {
-        T[] primeArray = new T[0];
+        T[] primeArray;
+        int capacity = 4;
+        int count = 0; 
+        public BakkenList()
+        {
+           T[] primeArray = new T[capacity];
+        }
+        public void Add(T newbie)
+        {
+            T[] birthArray = new T[count+capacity];
+            for (int i = 0; i < count; i++)
+            {
+                birthArray[i] = primeArray[i];
+            }
+            birthArray[count] = newbie;
+            count++;
+            primeArray = birthArray;
+        }
     }
 }
